eb0769df1804a05f611556a81cec4d388ec71cd2
Ryan Thompson <service@aiwebsystems.com>
2015-01-19 13:38:36 -0600

    mand

---
+++ src/MaintenanceModule.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\MaintenanceModule;
+
+use Anomaly\Streams\Platform\Addon\Module\Module;
+
+/**
+ * Class MaintenanceModule
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\MaintenanceModule
+ */
+class MaintenanceModule extends Module
+{
+
+}
